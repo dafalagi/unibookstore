c914776ea5a7e8f1fbacc173e2779a59e4f52f54
dafalagi <dafarizky34@gmail.com>
2023-02-03 18:46:22 +0700

    fixed sidebar

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
         <li class="nav-item">
           <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page" href="/dashboard">
             <span data-feather="home" class="align-text-bottom"></span>
-            Dashboard
+            Home
           </a>
         </li>
         <li class="nav-item">
